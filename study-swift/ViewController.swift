07ec92106e37c401830b440be807ccbcdc5f4621
kurakuramikan <m.kurahashi@diamondmedia.co.jp>
2020-08-10 00:17:24 +0900

    [Add] Initial commit

---
+++ study-swift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  study-swift
+//
+//  Created by 倉橋将幸 on 2020/08/10.
+//  Copyright © 2020 kurakuramikan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func log() {
+        print("test")
+    }
+
+}
+
